61e9f5782f844acf7211c1af37bc2e9f47d8fa81
Aladin BKZ <yayalatif1999@gmail.com>
2020-03-19 17:42:58 +0100

    separation des vues

---
+++ resources/views/sidebar.blade.php
@@ -0,0 +1,62 @@
+<aside class="col-12 col-md-2 p-0 bg-dark">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark flex-md-column flex-row align-items-start">
+      <div class="collapse navbar-collapse">
+        <ul class="flex-md-column flex-row navbar-nav w-100 justify-content-between">
+          <li class="nav-item active  ">
+            <a class="nav-link" href="{{ url('/home') }}">
+              <i class="material-icons">dashboard</i>
+              <p>Accueil</p>
+            </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="../template.blade.php">
+              <i class="material-icons">content_paste</i>
+              <p>Membres du club</p>
+            </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="./typography.html">
+              <i class="material-icons">library_books</i>
+              <p>Typography</p>
+            </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="./icons.html">
+              <i class="material-icons">bubble_chart</i>
+              <p>Icons</p>
+            </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="./map.html">
+              <i class="material-icons">location_ons</i>
+              <p>Maps</p>
+            </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="./notifications.html">
+              <i class="material-icons">notifications</i>
+              <p>Notifications</p>
+            </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="./rtl.html">
+              <i class="material-icons">language</i>
+              <p>RTL Support</p>
+            </a>
+          </li>
+          <li class="nav-item active-pro ">
+            <a class="nav-link" href="./upgrade.html">
+              <i class="material-icons">unarchive</i>
+              <p>Upgrade to PRO</p>
+            </a>
+          </li>            
+          <li class="nav-item ">
+            <a class="nav-link" href="./user.html">
+              <i class="material-icons">person</i>
+              <p>User Profile</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </aside>
